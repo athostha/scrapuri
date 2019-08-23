1d41dc51491ec7dc0de7832303c1a4c2d5996201
Athos Tavares Hounsell Almeida <33376789+athostha@users.noreply.github.com>
2019-08-23 12:18:41 -0300

    Update scrapuri.py
    Organized and updated

--- scrapuri.py
+++ scrapuri.py
@@ -1,43 +1,40 @@
-import urllib.request
-import csv
-
-ll = []
-print('Entre com a hash do seu Usuário:')
-minhahash = input()
-for i in range(1,20):
+#returns page as string
+def readpage(page):
     try:
-        page = "https://www.urionlinejudge.com.br/judge/pt/profile/"+minhahash+"?page=" + str(i)
-        with urllib.request.urlopen(page) as url:
-            s = url.read()
-        tt = str(s)
-        
-        while tt.find('<tr class="par">') != -1 or tt.find('<tr class="impar">') != -1:
-            if tt.find('<tr class="par">')<tt.find('<tr class="impar">'):
-                tt = tt[tt.find('<tr class="par">')+86:]
-            else:
-                tt = tt[tt.find('<tr class="impar">')+88:]
+        s = ur.Request(page, headers={'User-Agent': 'Mozilla/5.0'})
+        tt = ur.urlopen(s).read()
+        return str(tt)
+    except HTTPError:
+        return " "
+#returns the problem ids from the profile page
+def numberseparator(tt):
+    ll = []
+    while tt.find('<tr class="par">') != -1 or tt.find('<tr class="impar">') != -1:
+        if tt.find('<tr class="par">')<tt.find('<tr class="impar">'):
+            tt = tt[tt.find('<tr class="par">')+86:]
+        else:
+            tt = tt[tt.find('<tr class="impar">')+88:]
             if tt[:4].isdigit():
-                ll.append(tt[:4])
-    except urllib.error.HTTPError as e:
-        ok = True
-
-l = [['No','Tipo', 'feito','Pontos', 'Nível', 'Submissões', 'Resolveram', 'Relação', 'link']]
-for i in range(1001,2922):
-    try:
+                if tt[:4] not in ll:
+                    ll.append(tt[:4])
+    return ll
+#return list of all problems with various status
+def listall(ll):
+    l = [['No','Tipo', 'feito','Pontos', 'Nível', 'Submissões', 'Resolveram', 'Relação', 'link']]
+    for i in range(1001,2949):
         page = "https://www.urionlinejudge.com.br/judge/pt/problems/view/" + str(i)
-        with urllib.request.urlopen(page) as url:
-            s = url.read()
-        tt = str(s)
+        tt = readpage(page)
         t = tt[tt.find('<em class="points">') : tt.find('div class="main-content"')]
-        pontos = t[21:t.find(' POINTS')]
+        if t.find(' PONTOS') != -1:
+            pontos = t[21:t.find(' PONTOS')]
+        else:
+            pontos = t[21:t.find(' POINTS')]
         t = t[t.find('target="_blank">')+16:]
         tipo = t[:t.find('</a')]
         
         
-        page2 = "https://www.urionlinejudge.com.br/judge/pt/ranks/problem/" + str(i)
-        with urllib.request.urlopen(page2) as url:
-            s2 = url.read()
-        tt2 = str(s2)
+        page2 = "https://www.urionlinejudge.com.br/judge/pt/ranks/problem/" + str(i) + "/2"
+        tt2 = readpage(page2)
         tt2 = tt2[tt2.find('id="problem-statistics">')+24:]
         tt2 = tt2[tt2.find('<p>'):tt2.find('</div>')]
         resolveram = tt2[3:tt2.find('</p>')]
@@ -54,17 +51,45 @@ for i in range(1001,2922):
             l.append([str(i), str(tipo), str(1), str(pontos), str(nivel), str(submissoes), str(resolveram), str(relacao), page])
         else:
             l.append([str(i), str(tipo), str(0), str(pontos), str(nivel), str(submissoes), str(resolveram), str(relacao), page])
-        print(l[len(l)-1])
-    except urllib.error.HTTPError as e:
-        ok = True
+        progress(i-1000,1949)
+    return l
+#gives progress bar. from https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
 
-buffer = l
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
-for row_index, list in enumerate(buffer):
-    for column_index, string in enumerate(list):
-        buffer[row_index][column_index] = buffer[row_index][column_index].replace('\n', '')
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
-with open('output.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(buffer)
-#I'm guessing this would output the html source code?
+
+import urllib.request as ur
+from urllib.error import HTTPError
+import csv
+import sys
+
+
+def main():
+    ll = []
+    #Entre com a hash do seu Usuário
+    minhahash = "211459"
+    for i in range(1,20):
+        page = "https://www.urionlinejudge.com.br/judge/pt/profile/"+minhahash+"?page=" + str(i)
+        tt = readpage(page)
+        ll = ll + numberseparator(tt)
+    
+    buffer = listall(ll)
+    
+    #generates csv file
+    for row_index, list in enumerate(buffer):
+        for column_index, string in enumerate(list):
+            buffer[row_index][column_index] = buffer[row_index][column_index].replace('\n', '')
+    
+    with open('output.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(buffer)
+        
+if __name__ == '__main__':
+    main()
